c59f9bcfb805776a6223118689b1227e7102b1f8
Azure Admin <60490454+AzureTestLabs@users.noreply.github.com>
2023-02-07 10:58:22 -0700

    index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cool Test App</title>
+</head>
+<body>
+<h1></h1>
+<p>Cool Test App</p>
+<img src="https://github.com/AzureTestLabs/coolapp/blob/main/azure.jpg?raw=true" alt="cool image">
+</body>
+</html>
